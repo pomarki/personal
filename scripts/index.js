cac8f2224595b9e8be9caffba86f29b24e720d96
Denis Shalaev <pomarki@narod.ru>
2023-01-18 13:34:03 +0300

    01

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,32 @@ const aboutBtn = document.querySelector(".start__link_about");
 const worksBtn = document.querySelector(".start__link_works");
 const resumeBtn = document.querySelector(".start__link_resume");
 const linksBtn = document.querySelector(".start__link_links");
+const headerStateBtn = document.querySelector(".header__button-state");
+
+const headerSection = document.querySelector(".header");
+const headerLinks = document.querySelectorAll(".header__link");
+const headerBtns = document.querySelectorAll(".header__button");
+const headerLinksArr = Array.from(headerLinks);
+const headerBtnsArr = Array.from(headerBtns);
+
+const toggleHeader = () => {
+  headerStateBtn.classList.toggle("header__button-state_close");
+  headerSection.classList.toggle("header_type_close");
+
+  const blockToggle = (arr, ignore) => {
+    arr.forEach((item, index) => {
+      if (index === ignore) {
+        return;
+      } else {
+        item.classList.toggle("header_invisible-element");
+      }
+    });
+  };
+  blockToggle(headerLinksArr, 0);
+  blockToggle(headerBtnsArr, 0);
+};
+
+headerStateBtn.addEventListener("click", toggleHeader);
 
 aboutBtn.addEventListener("mouseover", (e) => {
   mainContainer.classList = "start__container";

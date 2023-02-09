8bee35a974b6f5eee902832c21b238f0fbcb9ea8
Denis Shalaev <pomarki@narod.ru>
2023-02-09 18:50:39 +0300

    01

--- scripts/data.js
+++ scripts/data.js
@@ -24,10 +24,10 @@ const morfingConst = [
     { id: 3, width: cV.m, top: cV.e, left: cV.s, rotate: cV.rd },
   ],
   [
-    { id: 0, width: cV.s, top: cV.e, left: cV.z, rotate: cV.rd },
+    { id: 0, width: cV.e, top: cV.e, left: cV.z, rotate: cV.ed },
     { id: 1, width: cV.xl, top: cV.z, left: cV.z, rotate: cV.ed },
-    { id: 2, width: cV.m, top: cV.e, left: cV.s, rotate: cV.rd },
-    { id: 3, width: cV.e, top: cV.e, left: cV.e, rotate: cV.ed },
+    { id: 2, width: cV.m, top: cV.e, left: cV.e, rotate: cV.rd },
+    { id: 3, width: cV.s, top: cV.e, left: cV.l, rotate: cV.rd },
   ],
   [
     { id: 0, width: cV.s, top: cV.z, left: cV.z, rotate: cV.rd },
